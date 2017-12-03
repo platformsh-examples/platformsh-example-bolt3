0b57c914c19c2324e7dad89633ce703039bb0558
Royall Spence <royall@royall.us>
2017-12-02 20:02:02 -0500

    Disable debug and set mail config explicitly

--- platform_config.php
+++ platform_config.php
@@ -3,7 +3,6 @@
 require_once __DIR__ . '/vendor/autoload.php';
 
 if (!empty($_ENV['PLATFORM_RELATIONSHIPS'])){
-    echo 'Writing Platform.sh database config...';
     $relationships = json_decode(base64_decode($_ENV['PLATFORM_RELATIONSHIPS']), true);
 
     $config = [
@@ -13,9 +12,13 @@ if (!empty($_ENV['PLATFORM_RELATIONSHIPS'])){
             'username' => $relationships['database'][0]['username'],
             'password' => $relationships['database'][0]['password'],
             'host' => $relationships['database'][0]['host']
+        ],
+        'debug' => 'false',
+        'mailoptions' => [
+            'transport' => 'mail',
+            'spool' => 'false'
         ]
     ];
 
     file_put_contents('app/config/config_local.yml', \Symfony\Component\Yaml\Yaml::dump($config));
-    echo "done!\n";
 }
